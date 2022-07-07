51105cb0427dad6b1c35b47ce784fd8238eee20b
quangkhatran <quangkhatran@gmail.com>
2022-07-07 16:16:17 +0700

    connect Sellers App to Firebase, init Firebase in App

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:sellers_app/splashScreen/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
