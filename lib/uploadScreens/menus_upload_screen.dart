48c003d1cc9f85d5dc2862beef8858037f6c0b87
quangkhatran <quangkhatran@gmail.com>
2022-07-10 15:53:57 +0700

    Capture Image with Camera, Pick Image from Gallery

--- lib/uploadScreens/menus_upload_screen.dart
+++ lib/uploadScreens/menus_upload_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../global/global.dart';
 
@@ -12,6 +13,9 @@ class MenusUploadScreen extends StatefulWidget {
 }
 
 class _MenusUploadScreenState extends State<MenusUploadScreen> {
+  XFile? imageXFile;
+  final ImagePicker _picker = ImagePicker();
+
   defaultScreen() {
     return Scaffold(
       appBar: AppBar(
@@ -87,12 +91,83 @@ class _MenusUploadScreenState extends State<MenusUploadScreen> {
                   ),
                 ),
               ),
-              onPressed: () {},
+              onPressed: () {
+                takeImage(context);
+              },
             ),
           ],
         )),
       ),
     );
+  }
+
+  takeImage(mContext) {
+    return showDialog(
+        context: mContext,
+        builder: (context) {
+          return SimpleDialog(
+            title: const Text(
+              'Menu Image',
+              style: TextStyle(
+                color: Colors.amber,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            children: [
+              SimpleDialogOption(
+                child: const Text(
+                  'Capture with Camera',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                onPressed: captureImageWithCamera,
+              ),
+              SimpleDialogOption(
+                child: const Text(
+                  'Select from Gallery',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                onPressed: pickImageFromGallery,
+              ),
+              SimpleDialogOption(
+                child: const Text(
+                  'Cancel',
+                  style: TextStyle(
+                    color: Colors.red,
+                  ),
+                ),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          );
+        });
+  }
+
+  captureImageWithCamera() async {
+    Navigator.pop(context);
+    imageXFile = await _picker.pickImage(
+      source: ImageSource.camera,
+      maxHeight: 720,
+      maxWidth: 1280,
+    );
+    setState(() {
+      imageXFile;
+    });
+  }
+
+  pickImageFromGallery() async {
+    Navigator.pop(context);
+    imageXFile = await _picker.pickImage(
+      source: ImageSource.gallery,
+      maxHeight: 720,
+      maxWidth: 1280,
+    );
+    setState(() {
+      imageXFile;
+    });
   }
 
   @override
